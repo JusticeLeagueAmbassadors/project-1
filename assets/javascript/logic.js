8a1991eec7c50c0ff988bfbe862197af76dcfc20
Daniel Owens <DanielOwens@Daniel-Owenss-MacBook-Pro-2.local>
2018-04-10 20:39:37 -0700

    initial update

---
+++ assets/javascript/logic.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    var submitIcon = $('.searchbox-icon');
+    var inputBox = $('.searchbox-input');
+    var searchBox = $('.searchbox');
+    var isOpen = false;
+    submitIcon.click(function(){
+        if(isOpen == false){
+            searchBox.addClass('searchbox-open');
+            inputBox.focus();
+            isOpen = true;
+        } else {
+            searchBox.removeClass('searchbox-open');
+            inputBox.focusout();
+            isOpen = false;
+        }
+    });  
+    submitIcon.mouseup(function(){
+        return false;
+    });
+    searchBox.mouseup(function(){
+        return false;
+    });
+    $(document).mouseup(function(){
+        if(isOpen == true){
+            $('.searchbox-icon').css('display','block');
+            submitIcon.click();
+        }
+    });
+});
+function buttonUp(){
+    var inputVal = $('.searchbox-input').val();
+    inputVal = $.trim(inputVal).length;
+    if( inputVal !== 0){
+        $('.searchbox-icon').css('display','none');
+    } else {
+        $('.searchbox-input').val('');
+        $('.searchbox-icon').css('display','block');
+    }
+}
+
+function initMap(){
+    var location = {
+        lat: 37.773972,
+        lng: -122.431297
+    };
+    var map = new google.maps.Map(document.getElementById("map"),{
+        zoom: 4, 
+        center: location
+
+    });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+        });
+    }
